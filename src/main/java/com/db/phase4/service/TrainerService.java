9f2f28f5276acc76c2470388c09bbeb67ecf3be5
이재현 <fanta4715@naver.com>
2023-12-03 15:07:08 +0900

    feat: Trainer View 스켈레톤 구현

---
+++ src/main/java/com/db/phase4/service/TrainerService.java
@@ -0,0 +1,18 @@
+package com.db.phase4.service;
+
+import com.db.phase4.dao.TrainerDao;
+import com.db.phase4.dto.trainer.TrainerViewDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrainerService {
+    private final TrainerDao trainerDao;
+
+
+    public List<TrainerViewDto> findByGymId(int gymId) {
+        return trainerDao.findByGymId(gymId);
+    }
+}
