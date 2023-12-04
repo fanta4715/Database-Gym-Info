e6d91ed3200c8e02831c0f4db86a546fb10a67b4
이재현 <fanta4715@naver.com>
2023-12-04 15:40:34 +0900

    feat: 특정 GYM에 해당하는 User 조회 기능 구현

--- src/main/java/com/db/phase4/dao/UserDao.java
+++ src/main/java/com/db/phase4/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.db.phase4.dao;
 
+import com.db.phase4.dto.UserViewDto;
 import com.db.phase4.dto.trainer.FilteredTrainerViewDto;
 import com.db.phase4.dto.trainer.TrainerRegisterDto;
 import com.db.phase4.util.ConnectionMaker;
@@ -116,6 +117,47 @@ public class UserDao {
         } finally {
             connectionMaker.closeAll(conn, stmt, rs);
             return myTrainer;
+        }
+    }
+
+    public List<UserViewDto> findByGymId(int gymId) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        List<UserViewDto> people = new ArrayList<>();
+
+        try {
+            conn = connectionMaker.createConnection();
+            stmt = conn.createStatement();
+
+            StringBuffer sb = new StringBuffer();
+            sb.append("SELECT Name as USER_NAME, TO_CHAR(Birth_date,'YYYY-MM-DD') as USER_BIRTHDAY, sex ");
+            sb.append("FROM ( ");
+            sb.append("    SELECT U.Name, U.Birth_date, E.Gym_id, U.sex ");
+            sb.append("    FROM USERS U JOIN ENROLLS E ON U.User_id = E.User_id ) ");
+            sb.append("WHERE Gym_id =" + gymId);
+
+            rs = stmt.executeQuery(sb.toString());
+            while (rs.next()){
+                String name = rs.getString(1);
+                LocalDate birthDate = rs.getDate(2).toLocalDate();
+                String sex = rs.getString(3);
+                UserViewDto user = UserViewDto.builder()
+                        .name(name)
+                        .birthdate(birthDate)
+                        .sex(sex)
+                        .build();
+
+                people.add(user);
+            }
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connectionMaker.closeAll(conn, stmt, rs);
+            return people;
         }
     }
 }
